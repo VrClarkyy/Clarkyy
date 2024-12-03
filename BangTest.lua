dbe5d1c19bae10ec70a82bd7e3d38f6772b4a3a5
VrClarkyy <strikentjff@gmail.com>
2024-12-02 21:45:25 -0300

    BangTest.lua

--- BangTest.lua
+++ BangTest.lua
@@ -814,7 +814,7 @@ Tab:AddToggle({
 
 Tab:AddToggle({
     Name = "No Ping",
-    Default = false,
+    Default = true,
     Callback = function(state)
         noPingEnabled = state  -- Atualiza o estado de noPing
         print("No Ping " .. (noPingEnabled and "Enabled" or "Disabled"))
